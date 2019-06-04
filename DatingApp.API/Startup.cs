14dc1b4e7542eb61e1e4e4ce4820af84056f0d2c
Rama Adarsh <ramaadarsh5@gmail.com>
2019-06-04 12:37:07 +0530

    Initial Commit

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -36,7 +36,7 @@ namespace DatingApp.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            // if (env.IsDevelopment())
+             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
