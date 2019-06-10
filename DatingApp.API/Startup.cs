4519b930f9b64c0211b1e3f31063fb098f1697b4
Rama Adarsh <ramaadarsh5@gmail.com>
2019-06-10 10:51:37 +0530

    Commit

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -59,6 +59,8 @@ namespace DatingApp.API
             }
             else
             {
+
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
               //  app.UseHsts();
             }
